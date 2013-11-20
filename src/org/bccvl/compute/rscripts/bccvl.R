8b3a8016315e26dd09dbf6a3d20e2bf1a0afe718
Gerhard Weis <g.weis@griffith.edu.au>
2013-11-20 14:33:14 +1000

    commit missing bccvl.R script

---
+++ src/org/bccvl/compute/rscripts/bccvl.R
@@ -0,0 +1,112 @@
+# setup R environment
+if (!file.exists(Sys.getenv("R_LIBS_USER"))) {
+    dir.create(Sys.getenv("R_LIBS_USER"), recursive=TRUE);
+}
+.libPaths(Sys.getenv("R_LIBS_USER"))
+# set CRAN mirror in case we need to download something
+
+r <- getOption("repos")
+r["CRAN"] <- "http://cran.ms.unimelb.edu.au/"
+options(repos=r)
+
+
+#script to run to develop distribution models
+###check if libraries are installed, install if necessary and then load them
+necessary=c("rjson", "dismo","SDMTools", "gbm", "rgdal", "pROC", "R2HTML", "png", "biomod2") #list the libraries needed
+installed = necessary %in% installed.packages() #check if library is installed
+if (length(necessary[!installed]) >=1) {
+    install.packages(necessary[!installed], dep = T) #if library is not installed, install it
+}
+for (lib in necessary) {
+    library(lib,character.only=T) #load the libraries
+}
+
+# load parameters
+bccvl.params <- rjson::fromJSON(file="params.json")
+# set working directory (script runner takes care of it)
+setwd(bccvl.params$outputdir)
+
+############################################################
+#
+# define helper functions to use in bccvl
+#
+############################################################
+
+## Needed for tryCatch'ing:
+bccvl.err.null <- function (e) return(NULL)
+
+# read species presence/absence data
+#    return NULL if filename is not  given
+bccvl.species.read <- function(filename) {
+    if (!is.null(filename)) {
+        return (read.csv(filename))
+    }
+}
+
+# function to save projection output raster
+bccvl.saveModelProjection <- function(model.obj, projection.name, outputdir=bccvl.params$outputdir) {
+    filename = file.path(outputdir, paste(projection.name, '.tif', sep=""))
+    writeRaster(model.obj, filename, format="GTiff", options="COMPRESS=LZW", overwrite=TRUE)
+}
+
+# function to save RData in outputdir
+bccvl.save <- function(robj, name, outputdir=bccvl.params$outputdir) {
+    filename = file.path(outputdir, name)
+    save(robj, file=filename)
+}
+
+# function to save CSV Data in outputdir
+bccvl.write.csv <- function(robj, name, outputdir=bccvl.params$outputdir) {
+    filename = file.path(outputdir, name)
+    write.csv(robj, file=filename)
+}
+
+# function to get model object
+bccvl.getModelObject <- function(model.name, inputdir=bccvl.params$inputdir) {
+    filename = file.path(inputdir, paste(model.name, '.RData', sep=""))
+    model.obj = tryCatch(get(load(file=filename)), error = err.null)
+    return (model.obj)
+}
+
+############################################################
+#
+# define helper functions for projections
+#
+############################################################
+
+# function to check that the environmental layers used to project the
+# model are the same as the ones used to create the model object
+#    model.obj     ... model to project
+#    climatelayers ... climate data to project onto
+bccvl.checkModelLayers = function(model.obj, climatelayers) {
+    message("Checking environmental layers used for projection")
+    # get the names of the environmental layers from the original model
+    if (inherits(model.obj, "DistModel")) {
+        # dismo package
+        model.layers = colnames(model.obj@presence)
+    } else if (inherits(model.obj, "gbm")) {
+        # brt package
+        model.layers = summary(model.obj)$var
+    } else if (inherits(model.obj, "BIOMOD.models.out")) {
+        # biomod package
+        model.layers = model.obj@expl.var.names
+    }
+
+    # get the names of the climate scenario's env layers
+    pred.layers = names(climatelayers)
+
+    # check if the env layers were in the original model
+    if(sum(!(pred.layers %in% model.layers)) > 0 ){
+        message("Dropping environmental layers not used in the original model creation...")
+        # create a new list of env predictors by dropping layers not in the original model
+        new.predictors = climatelayers
+        for (pl in pred.layers) {
+            if (!(pl %in% model.layers)) {
+                new.predictors = dropLayer(new.predictors, pl)
+            }
+        }
+        return(new.predictors)
+    } else {
+        return(climatelayers)
+    }
+}
