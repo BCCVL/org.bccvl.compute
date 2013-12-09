02ddc5791a69d627fa02652e735bf720870c7a59
Gerhard Weis <g.weis@griffith.edu.au>
2013-12-10 09:22:44 +1000

    added projection functions

---
+++ src/org/bccvl/compute/predict.py
@@ -0,0 +1,84 @@
+from pkg_resources import resource_string
+from org.bccvl.compute.utils import WorkEnv, WorkEnvLocal, queue_job, getdatasetparams
+from gu.z3cform.rdf.interfaces import IGraph
+from org.bccvl.site.namespace import BIOCLIM
+from plone.app.uuid.utils import uuidToObject
+from zope.interface import moduleProvides
+# do this dynamically in site module?
+from .interfaces import IComputeFunction
+
+moduleProvides(IComputeFunction)
+
+
+def get_project_params(experiment):
+    params = {'sdms': {},
+              'climate': {}}
+    uuid = experiment.species_distribution_models,
+    params['sdms'][uuid] = getdatasetparams(uuid)
+
+    sdmobj = uuidToObject(uuid)
+    graph = IGraph(sdmobj)
+    layers = list(graph.objects(graph.identifier, BIOCLIM['bioclimVariable']))
+    params['layersperdataset'] = layers
+
+    for uuid in experiment.future_climate_datasets():
+        params['climate'][uuid] = getdatasetparams(uuid)
+        params['climate'][uuid]['archivefiles'] = [
+            ]
+    params['datasetkeys'] = ('sdms', 'climate')
+    return params
+
+
+def generate_project_script():
+    script = '\n'.join([
+        resource_string('org.bccvl.compute', 'rscripts/bccvl.R'),
+        resource_string('org.bccvl.compute', 'rscripts/predict.R'),
+    ])
+    return script
+
+OUTPUTS = {
+    'files': {
+        '*.tif': {
+            'title': 'Projection',
+            'type': 'projection',
+            'format': 'GTiff',  # TODO: replace format withe mime/type?
+        },
+    },
+    'archives': {
+        # 'results.html.zip': {
+        #     'files': ['results.html', 'AUC.png'],
+        #     'title': 'Accuracy measures report as zip',
+        #     'type': 'eval',
+        #     'format': 'zip',
+    },
+}
+
+
+def execute(experiment, request=None, workenv=WorkEnv):
+    """
+    This function takes an experiment and executes.
+
+    It usesenvirnoment variables WORKER_DIR or HOME as root folder to execute
+    experiments.
+
+    After the execution finishes the output files will be attached to the
+    experiment.
+
+    :param experiment: The experiment holding the configuration and receiving
+                       the results
+    :type experiment: org.bccvl.site.content.IExperiment
+
+
+    """
+    # TODO: CREATE WorkEnv in job
+    # workenv = WorkEnvLocal
+    env = workenv('localhost', request)
+    params = get_project_params(experiment)
+    script = generate_project_script()
+    return queue_job(experiment, 'Projection', env, script, params, OUTPUTS)
+
+
+parameters = None
+
+if __name__ == '__main__':
+    execute()
--- src/org/bccvl/compute/rscripts/predict.R
+++ src/org/bccvl/compute/rscripts/predict.R
@@ -14,75 +14,95 @@
 # dismo places outputfiles in outputdir
 # biomod places outputfiles in subfolders in outputdir
 
-projection.name <- "future"
 
-future.climate.scenario <- stack(bccvl.params$future$data)
+
+projectdataset <- function(model.obj, futuredata, projection.name) {
+    future.climate.scenario = stack(futuredata)
+    # filter out unused layers from future.climate.scenario
+    predictors <- bccvl.checkModelLayers(model.obj, future.climate.scenario)
+    # do projection
+    if (inherits(model.obj, "DistModel")) {
+        # dismo package
+        opt.tails <- bccvl.params$tails
+        opt.ext <- NULL
+        model.proj <- predict(model.obj,
+                              predictors,
+                              tails=opt.tails,
+                              ext=opt.ext)
+        bccvl.saveModelProjection(model.proj, projection.name)
+    } else if (inherits(model.obj, "gbm")) {
+        # brt package)
+        model.proj <- predict(predictors,
+                              model.obj,
+                              n.trees=model.obj$gbm.call$best.trees,
+                              type="response")
+        bccvl.saveModelProjection(model.proj, projection.name)
+    } else if (inherits(model.obj, "BIOMOD.models.out")) {
+        # expect additional model data in input folder.
+        # for biomod to find it we'll have to change wd
+
+        biomod.xy.new.env <- NULL
+        biomod.selected.models <- "all" # bccvl.params$selected_models
+        biomod.binary.meth <- NULL
+        biomod.filtered.meth <- NULL
+        biomod.compress <- bccvl.params$compress
+        biomod.build.clamping.mask <- TRUE
+        biomod.species.name <- "species" # bccvl.params$species
+        opt.biomod.silent <- FALSE
+        opt.biomod.do.stack <- TRUE
+        opt.biomod.keep.in.memory <- TRUE
+        opt.biomod.output.format <- NULL
+
+        model.proj <- BIOMOD_Projection(modeling.output=model.obj,
+                                        new.env=predictors,
+                                        proj.name=projection.name,
+                                        xy.new.env=biomod.xy.new.env,
+                                        selected.models=biomod.selected.models,
+                                        binary.meth=biomod.binary.meth,
+                                        filtered.meth=biomod.filtered.meth,
+                                        # compress=biomod.compress, # .. Null not accepted
+                                        build.clamping.mask=biomod.build.clamping.mask,
+                                        silent=opt.biomod.silent,
+                                        do.stack=opt.biomod.do.stack,
+                                        keep.in.memory=opt.biomod.keep.in.memory,
+                                        output.format=opt.biomod.output.format)
+        # save projection to output folder
+        # move proj_folder
+        projinput <- file.path(getwd(),
+                               biomod.species.name,
+                               paste("proj", projection.name, sep="_"))
+        projoutput <- file.path(bccvl.params$outputdir,
+                                biomod.species.name,
+                                paste("proj", projection.name, sep="_"))
+        # create top level dir
+        dir.create(file.path(bccvl.params$outputdir, biomod.species.name))
+        # move proj_future folder to output folder
+        file.rename(projinput, projoutput)
+        # convert grd files to tif
+        bccvl.grdtogtiff(projoutput)
+    }
+}
+
+
+# TODO: get rid of this zip detection
+modelfile <- bccvl.params$sdms[1]
+if (tolower(file_ext(modelfile)) == "zip") {
+    # assume we have been given a zip file and the model file has the same name
+    # as the basename(zip) within a subfolder with the same name
+    zipdir = file_path_sans_ext(modelfile)
+    zipfile = basename(zipdir)
+    modelfile = file.path(zipdir, zipfile)
+    # we'll have to change wd so that biomod can load data from model subfolders'
+    setwd(zipdir)
+}
+
 
 # load model
-model.obj <- bccvl.getModelObject(bccvl.params$inputmodel)
+# TODO:should be loaded straigt from bccvl.params$sdms[1]
+model.obj <- bccvl.getModelObject(modelfile)
 
-# filter out unused layers from future.climate.scenario
-predictors <- bccvl.checkModelLayers(model.obj, future.climate.scenario)
-
-# do projection
-if (inherits(model.obj, "DistModel")) {
-    # dismo package
-    opt.tails <- bccvl.params$tails
-    opt.ext <- NULL
-    model.proj <- predict(model.obj,
-                          predictors,
-                          tails=opt.tails,
-                          ext=opt.ext)
-    bccvl.saveModelProjection(model.proj, projection.name)
-} else if (inherits(model.obj, "gbm")) {
-    # brt package)
-    model.proj <- predict(predictors,
-                          model.obj,
-                          n.trees=model.obj$gbm.call$best.trees,
-                          type="response")
-    bccvl.saveModelProjection(model.proj, projection.name)
-} else if (inherits(model.obj, "BIOMOD.models.out")) {
-    # expect additional model data in input folder.
-    # for biomod to find it we'll have to change wd
-    setwd(bccvl.params$inputdir) # TODO: get foldername from bccvl.params$inputmodel
-
-    biomod.xy.new.env <- NULL
-    biomod.selected.models <- bccvl.params$selected_models
-    biomod.binary.meth <- NULL
-    biomod.filtered.meth <- NULL
-    biomod.compress <- bccvl.params$compress
-    biomod.build.clamping.mask <- TRUE
-    biomod.species.name <- bccvl.params$species
-    opt.biomod.silent <- FALSE
-    opt.biomod.do.stack <- TRUE
-    opt.biomod.keep.in.memory <- TRUE
-    opt.biomod.output.format <- NULL
-
-    model.proj <- BIOMOD_Projection(modeling.output=model.obj,
-                                    new.env=predictors,
-                                    proj.name=projection.name,
-                                    xy.new.env=biomod.xy.new.env,
-                                    selected.models=biomod.selected.models,
-                                    binary.meth=biomod.binary.meth,
-                                    filtered.meth=biomod.filtered.meth,
-                                    # compress=biomod.compress, # .. Null not accepted
-                                    build.clamping.mask=biomod.build.clamping.mask,
-                                    silent=opt.biomod.silent,
-                                    do.stack=opt.biomod.do.stack,
-                                    keep.in.memory=opt.biomod.keep.in.memory,
-                                    output.format=opt.biomod.output.format)
-    # save projection to output folder
-    # move proj_folder
-    projinput <- file.path(getwd(),
-                           biomod.species.name,
-                           paste("proj", projection.name, sep="_"))
-    projoutput <- file.path(bccvl.params$outputdir,
-                            biomod.species.name,
-                            paste("proj", projection.name, sep="_"))
-    # create top level dir
-    dir.create(file.path(bccvl.params$outputdir, biomod.species.name))
-    # move proj_future folder to output folder
-    file.rename(projinput, projoutput)
-    # convert grd files to tif
-    bccvl.grdtogtiff(projoutput)
+for (dataset in bccvl.params$climate) {
+    # use folder name of first dataset to generate name for projection output
+    projection.name = basename(dirname(dataset[1]))
+    projectdataset(model.obj, dataset, projection.name)
 }
