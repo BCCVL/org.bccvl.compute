b2a4ddf79ee0d9b739c5c79fa886c83d53b9a59b
Gerhard Weis <g.weis@griffith.edu.au>
2014-04-10 15:05:26 +1000

    maxent needs to be deployed on compute machine

--- src/org/bccvl/compute/utils.py
+++ src/org/bccvl/compute/utils.py
@@ -161,7 +161,6 @@ class WorkEnv(object):
         self.scriptname = None
         self.wrapname = None
         self.jobid = None
-        self.maxentjarname = None
         self.src = 'plone'
         self.dest = 'compute'
         if host is None:
@@ -319,11 +318,6 @@ class WorkEnv(object):
         # TODO: We should only copy this if it's needed.
         # TODO: In bccvl.R, the working directory is configured to the output directory, hence we place the maxent.jar file there...
         #       This means that we get copies of this file in the output of each experiment.
-        self.maxentjarname = '/'.join((self.scriptdir, 'maxent.jar'))
-        src['path'] = resource_filename('org.bccvl.compute',
-                                        'rscripts/maxent.jar')
-        dest['path'] = self.maxentjarname
-        self.move_data('script', src, dest, None)
 
     def move_output_data(self):
         # TODO: use data_mover's transfer folder feature?'
