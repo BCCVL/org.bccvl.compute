3afff2063d51622d61327933cf3e9fe52823fec9
Gerhard Weis <g.weis@griffith.edu.au>
2014-06-30 18:00:10 +1000

    add species traits job execution

--- src/org/bccvl/compute/content/toolkit/lm/lm.R
+++ src/org/bccvl/compute/content/toolkit/lm/lm.R
@@ -4,50 +4,37 @@
 
 ## Get the data
 
-lm.data = read.table(bccvl.params$data[1], header=T, sep=",")
-
+lm.data = read.table(bccvl.params$data_table$filename, header=T, sep=",")
 
 ## Set up the function call expression
+lm.params = list(data=lm.data)
 
-lm.call = paste(
-        "lm(",
-        bccvl.params$formula[1], ",",
-        bccvl.params$data_table[1], ",",
-        "subset=", bccvl.params$subset[1], ",",
-        "weights=", bccvl.params$weights[1], ",",
-        "na.action=", bccvl.params$na_action[1], ",",
-        "method=\'", bccvl.params$method[1], "\',",
-        "model=", bccvl.params$model[1], ",",
-        "x=", bccvl.params$x[1], ",",
-        "y=", bccvl.params$y[1], ",",
-        "qr=", bccvl.params$qr[1], ",",
-        "singular.ok=", bccvl.params$singular.ok[1], ",",
-        "contrasts=", bccvl.params$contrasts[1], ",",
-        "offset=", bccvl.params$offset[1],
-        sep="")
+# scalar parameters
+for (paramname in c('formula', 'na.action', 'method', 'model', 'x', 'y', 'qr')) {
+    if (! is.null(bccvl.params[[paramname]])) {
+        lm.params[paramname] = bccvl.params[paramname]
+    }
+}
 
-if (bccvl.params$other_args[1] != "NULL") {
-  lm.call = paste( lm.call, bccvl.params$other_args[1], sep="")
-  }
+# parameters that sholud refer to a column in lm.data
+for (paramname in c('subset', 'weights', 'contrasts','offset')) {
+    if (! is.null(bccvl.params[[paramname]])) {
+        lm.params[paramname] = lm.data[bccvl.params[[paramname]]]
+    }
+}
 
-lm.call = paste( lm.call, ")", sep="" )
-
+# TODO: bccvl.params$other_params ... x, y, offset, method, tol, singular.ok
 
 ## Run the regression
 
-lm.result =
-  eval( parse( text=lm.call ) )
-
+lm.result = do.call(lm, lm.params)
 
 ## Save the result to file
 
 bccvl.save(lm.result, "lm.model.object.RData")
-
 
 ## Save result summary to a text file
 
 sink(file="lm_result_summary.txt")
 summary(lm.result)
 sink()
-
-
---
+++ src/org/bccvl/compute/sptraits.py
@@ -0,0 +1,83 @@
+from pkg_resources import resource_string
+from org.bccvl.compute.utils import getdatasetparams
+# do this dynamically in site module?
+from zope.interface import provider
+from org.bccvl.site.interfaces import IComputeMethod
+from copy import deepcopy
+from plone import api
+import tempfile
+from org.bccvl.tasks.compute import r_task
+from org.bccvl.tasks.plone import after_commit_task
+import logging
+import json
+
+LOG = logging.getLogger(__name__)
+
+
+def get_traits_params(result):
+    params = deepcopy(result.job_params)
+    # get metadata for species_distribution_models
+    uuid = params['data_table']
+    params['data_table'] = getdatasetparams(uuid)
+    # add hints for worker
+    workerhints = {
+        'files': ('data_table', )
+    }
+    return {'env': {}, 'params': params, 'worker': workerhints}
+
+
+def generate_traits_script(rscript):
+    script = '\n'.join([
+        resource_string('org.bccvl.compute', 'rscripts/bccvl.R'),
+        rscript,
+    ])
+    return script
+
+
+@provider(IComputeMethod)
+def execute(result, toolkit):
+    """
+    This function takes an experiment and executes.
+
+    It usesenvirnoment variables WORKER_DIR or HOME as root folder to execute
+    experiments.
+
+    After the execution finishes the output files will be attached to the
+    experiment.
+
+    :param experiment: The experiment holding the configuration and receiving
+                       the results
+    :type experiment: org.bccvl.site.content.IExperiment
+
+
+    """
+    try:
+        OUTPUTS = json.loads(toolkit.output)
+    except (ValueError, TypeError) as e:
+        LOG.fatal("couldn't load OUTPUT from toolkit %s: %s",
+                  toolkit.getId(), e)
+        OUTPUTS = {}
+    params = get_traits_params(result)
+    script = generate_traits_script(toolkit.script)
+    ### plone context for this job
+    member = api.user.get_current()
+    context = {
+        'context': '/'.join(result.getPhysicalPath()),
+        'user': {'id': member.getUserName(),
+                 'email': member.getProperty('email'),
+                 'fullname': member.getProperty('fullname')
+                 },
+        'experiment': {'title': result.__parent__.title,
+                       'url': result.__parent__.absolute_url()
+                       }
+    }
+    ### add result infos
+    params['result'] = {
+        'results_dir': tempfile.mkdtemp(),
+        'outputs': OUTPUTS
+    }
+    params['worker']['script'] = {
+        'name': '{}.R'.format(toolkit.getId()),
+        'script': script
+    }
+    after_commit_task(r_task, params, context)
