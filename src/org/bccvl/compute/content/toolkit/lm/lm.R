17f89b95dc348414f05a81d844f1317a87ea2ab9
Gerhard Weis <g.weis@griffith.edu.au>
2014-07-24 13:14:05 +1000

    update lm.R

--- src/org/bccvl/compute/content/toolkit/lm/lm.R
+++ src/org/bccvl/compute/content/toolkit/lm/lm.R
@@ -9,10 +9,25 @@ lm.data = read.table(bccvl.params$data_table$filename, header=T, sep=",")
 ## Set up the function call expression
 lm.params = list(data=lm.data)
 
+## set defaults for missing parameters according to R docs:
+lm.defaults = list(subset=NULL,
+                   weights=NULL,
+                   na.action=options("na.action")[[1]],
+                   method="qr",
+                   model=TRUE,
+                   x=FALSE,
+                   y=FALSE,
+                   qr=TRUE,
+                   singular.ok=TRUE,
+                   contrasts=NULL,
+                   offset=NULL)
+
 # scalar parameters
 for (paramname in c('formula', 'na.action', 'method', 'model', 'x', 'y', 'qr')) {
     if (! is.null(bccvl.params[[paramname]])) {
         lm.params[paramname] = bccvl.params[paramname]
+    } else {
+        lm.params[paramname] = lm.defaults[paramname]
     }
 }
 
@@ -20,14 +35,33 @@ for (paramname in c('formula', 'na.action', 'method', 'model', 'x', 'y', 'qr')) 
 for (paramname in c('subset', 'weights', 'contrasts','offset')) {
     if (! is.null(bccvl.params[[paramname]])) {
         lm.params[paramname] = lm.data[bccvl.params[[paramname]]]
+    } else {
+        lm.params[paramname] = lm.defaults[paramname]
     }
 }
 
-# TODO: bccvl.params$other_params ... x, y, offset, method, tol, singular.ok
+# singular.ok has a different name in bccvl.params
+if (! is.null(bccvl.params['singura_ok'])) {
+    lm.params['singular.ok'] = bccvl.params$singular_ok
+} else {
+    lm.params['singular.ok'] = lm.defaults$singular.ok
+}
 
 ## Run the regression
-
-lm.result = do.call(lm, lm.params)
+lm.result = lm(formula=lm.params$formula,
+               data=lm.params$data,
+               subset=lm.params$subset,
+               weights=lm.params$weigths,
+               na.action=lm.params$na.action[[1]],
+               method=lm.params$method,
+               model=lm.params$model,
+               x=lm.params$x,
+               y=lm.params$y,
+               qr=lm.params$qr,
+               singular.ok=lm.params$singular.ok,
+               contrasts=lm.params$contrasts,
+               offset=lm.params$offset
+               )
 
 ## Save the result to file
 
