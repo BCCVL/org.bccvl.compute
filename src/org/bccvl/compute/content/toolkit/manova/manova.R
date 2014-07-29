bff7c2328ebcab29c82cc05f5223812af07b7819
David Woolford <wolfwoolford@gmail.com>
2014-07-29 16:44:01 +1000

    STM related. Pushing function manova.R, note it is very similar to aov.R

--- src/org/bccvl/compute/content/toolkit/manova/manova.R
+++ src/org/bccvl/compute/content/toolkit/manova/manova.R
@@ -1,5 +1,57 @@
 
-### anova.R ###
+### manova.R ###
 
-print('Implement Me!')
+## Get the data
+
+manova.data = read.table(bccvl.params$data_table$filename, header=T, sep=",")
+
+## Set up the function call expression
+manova.params = list(data=manova.data)
+
+## set defaults for missing parameters according to R docs:
+manova.defaults = list(subset=NULL,
+                   weights=NULL,
+                   contrasts=NULL,
+                   na.action=options("na.action")[[1]],
+                   qr=TRUE,
+                   projections=FALSE)
+
+# plain old parameters
+for (paramname in c('formula', 'na.action', 'qr', "projections")) {
+    if (! is.null(bccvl.params[[paramname]])) {
+        manova.params[paramname] = bccvl.params[paramname]
+    } else {
+        manova.params[paramname] = manova.defaults[paramname]
+    }
+}
+
+# parameters that sholud refer to a column in manova.data
+for (paramname in c('subset', 'weights', 'contrasts')) {
+    if (! is.null(bccvl.params[[paramname]])) {
+        manova.params[paramname] = manova.data[bccvl.params[[paramname]]]
+    } else {
+        manova.params[paramname] = manova.defaults[paramname]
+    }
+}
+
+## Execute calculations
+manova.result = manova(formula=formula(manova.params$formula),
+                 data=manova.params$data,
+                 subset=manova.params$subset,
+                 weights=manova.params$weights,
+                 contrasts=manova.params$contrasts,
+                 na.action=manova.params$na.action[[1]],
+                 qr=manova.params$qr,
+                 projections=manova.params$projections,
+                 )
+
+## Save the result to file
+
+bccvl.save(manova.result, "manova.model.object.RData")
+
+## Save result summary to a text file
+
+sink(file="manova_result_summary.txt")
+summary(manova.result)
+sink()
 
