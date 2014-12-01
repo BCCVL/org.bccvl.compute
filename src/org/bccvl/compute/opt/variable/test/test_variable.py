0cc09862dba4e80f4b5bb16be932f36bd20d3eeb
David Woolford <wolfwoolford@gmail.com>
2014-12-01 15:03:30 +1100

    minor updates to test variable

--- src/org/bccvl/compute/opt/variable/test/test_variable.py
+++ src/org/bccvl/compute/opt/variable/test/test_variable.py
@@ -7,31 +7,35 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__))+"/../")
 import unittest
 import variable
 
-# Here's our "unit tests".
 class VariableTests(unittest.TestCase):
+    """ 
+        These tests dont do much, they just create the different objects and call
+        the important functions. Add print statements here and tinker
+    """ 
     
     def test_double_range_basic(self):
         import json
         d=json.loads(open("params.json").read())
         params = { "min": 0, "max": 1, "json_dict_path": ["params", "rang"], "n":10}
         s = variable.double_range(d, params)
-        #print s.every_choice()
-        #print s.random_choice()
+        s.every_choice()
+        s.random_choice()
     
     def test_integer_range_basic(self):
         import json
         d=json.loads(open("params.json").read())
         params = { "min": 10, "max": 100, "json_dict_path": ["params", "maxit"], "step":3}
         s = variable.integer_range(d, params)
-        #print s.every_choice()
-        #print s.random_choice()
+        s.every_choice()
+        s.random_choice()
 
     def test_subset_basic(self):
         import json
         d=json.loads(open("params.json").read())
         params = { "min": 1, "max": 1, "json_dict_path": ["params", "environmental_datasets"] }
         s = variable.subset(d, params)
-        #print s.every_choice()
+        s.every_choice()
+        s.random_choice()
 
 def main():
     unittest.main()
