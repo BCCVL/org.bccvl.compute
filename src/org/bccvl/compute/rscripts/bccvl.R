85f8f99a4fd3031aed73d469ac188ed37ea22e0f
Chantal Huijbers <c.huijbers@griffith.edu.au>
2015-09-11 10:53:48 +1000

    Update bccvl.R

--- src/org/bccvl/compute/rscripts/bccvl.R
+++ src/org/bccvl/compute/rscripts/bccvl.R
@@ -25,7 +25,7 @@ write.table(installed.packages()[,c("Package", "Version", "Priority")],
 
 #script to run to develop distribution models
 ###check if libraries are installed, install if necessary and then load them
-necessary=c("ggplot2","tools", "rjson", "dismo","SDMTools", "gbm", "rgdal", "pROC", "R2HTML", "png", "biomod2") #list the libraries needed
+necessary=c("ggplot2","tools", "rjson", "dismo","SDMTools", "gbm", "rgdal", "pROC", "R2HTML", "png", "gstat", "biomod2") #list the libraries needed
 installed = necessary %in% installed.packages() #check if library is installed
 if (length(necessary[!installed]) >=1) {
     install.packages(necessary[!installed], dep = T) #if library is not installed, install it
