f9006fcab6d2141491874a6b0cb611dd647e3e36
Gerhard Weis <g.weis@griffith.edu.au>
2015-11-25 22:53:27 +1000

    update result source in transmogrifier chain (support local and remote imports)

--- src/org/bccvl/compute/transmogrify.py
+++ src/org/bccvl/compute/transmogrify.py
@@ -1,7 +1,9 @@
 from itertools import chain
+from decimal import Decimal, InvalidOperation
 import mimetypes
 import tempfile
 import os.path
+import re
 from shutil import copyfileobj
 from collective.transmogrifier.interfaces import ISectionBlueprint
 from collective.transmogrifier.interfaces import ISection
@@ -13,7 +15,7 @@ from plone.i18n.normalizer.interfaces import IFileNameNormalizer
 from zope.component import getUtility
 from Products.CMFCore.utils import getToolByName
 from plone.app.uuid.utils import uuidToObject
-from urlparse import urlparse
+from urlparse import urlsplit
 import logging
 
 LOG = logging.getLogger(__name__)
@@ -61,6 +63,8 @@ def addSpeciesInfo(bccvlmd, result):
 @provider(ISectionBlueprint)
 @implementer(ISection)
 class ResultSource(object):
+    # FIXME: update to process passed in dict from worker
+    #        get rid of filematadata steps etc...
 
     def __init__(self, transmogrifier, name, options, previous):
         self.transmogrifier = transmogrifier
@@ -68,118 +72,111 @@ class ResultSource(object):
         self.name = name
         self.options = options
         self.previous = previous
-        self.outputmap = options.get('outputmap')
+        self.items = options.get('items')
 
         self.path = options['path'].strip()
-        if self.path is None or not os.path.isdir(self.path):
-            raise Exception(
-                'Directory ({}) does not exists.'.format(str(self.path)))
+        # if self.path is None or not os.path.isdir(self.path):
+        #     raise Exception(
+        #         'Directory ({}) does not exists.'.format(str(self.path)))
 
         # add path prefix to imported content
         self.prefix = options.get('prefix', '').strip().strip(os.sep)
         # keys for sections further down the chain
         self.pathkey = options.get('path-key', '_path').strip()
         self.fileskey = options.get('files-key', '_files').strip()
-        self.filesmd = options.get('outputmd', {})
 
-    def createItem(self, fname, info, thresholds):
+
+    def createItem(self, import_item):
         # fname: full path to file
         # info: 'title', 'type'
-        name = os.path.basename(fname)
-        mtr = getToolByName(self.context, 'mimetypes_registry')
+        url = import_item['file']['url']
+        name = import_item['file']['filename']
+        # mtr = getToolByName(self.context, 'mimetypes_registry')
         normalizer = getUtility(IFileNameNormalizer)
-
         datasetid = normalizer.normalize(name)
 
-        layermd = {}
-        bccvlmd = {}
-        genre = info.get('genre', None)
+        bccvlmd = import_item['bccvlmetadata']
 
+        genre = bccvlmd.get('genre', None)
         if genre:
-            bccvlmd['genre'] = genre
             # FIXME: attach species data to everything?
-
-            #  resolution, toolkit, species, layers
-            #  future: year, emsc, gcm
             if genre in ('DataGenreSDMModel', 'DataGenreCP', 'DataGenreClampingMask'):
-                # TODO: add layer info to info dictionary (comes from return value description)
-                # Add layer information for DataGenreClampingMask and DataGenreCP
-                if genre == 'DataGenreClampingMask':
-                    layermd = {'files': {name: {'layer': 'clamping_mask', 'data_type': 'Discrete'}}}
-                elif genre == 'DataGenreCP':
-                    if self.context.job_params['function'] in ('circles', 'convhull', 'voronoiHull'):
-                        layermd = {'files': {name: {'layer': 'projection_binary', 'data_type': 'Continuous'}}}
-                    elif self.context.job_params['function'] in ('maxent',):
-                        layermd = {'files': {name: {'layer': 'projection_suitability', 'data_type': 'Continuous'}}}
-                    else:
-                        layermd = {'files': {name: {'layer': 'projection_probability', 'data_type': 'Continuous'}}}
-
                 addLayersUsedInfo(bccvlmd, self.context)
                 bccvlmd['resolution'] = self.context.job_params['resolution']
                 addSpeciesInfo(bccvlmd, self.context)
             if genre == 'DataGenreEnsembleResult':
                 bccvlmd['resolution'] = self.context.job_params['resolution']
             elif genre == 'DataGenreFP':
-                # Add layer information
-                if self.context.job_params['function'] in ('circles', 'convhull', 'voronoiHull'):
-                    layermd = {'files': {name: {'layer': 'projection_binary', 'data_type': 'Continuous'}}}
-                elif self.context.job_params['function'] in ('maxent',):
-                    layermd = {'files': {name: {'layer': 'projection_suitability', 'data_type': 'Continuous'}}}
-                else:
-                    layermd = {'files': {name: {'layer': 'projection_probability', 'data_type': 'Continuous'}}}
                 addLayersUsedInfo(bccvlmd, self.context)
                 bccvlmd['resolution'] = self.context.job_params['resolution']
                 addSpeciesInfo(bccvlmd, self.context)
-
-                # FIXME: find a cleaner way to attach metadata
-                if 'year' in self.context.job_params:
-                    bccvlmd['year'] = self.context.job_params['year']
-                if 'month' in self.context.job_params:
-                    bccvlmd['month'] = self.context.job_params['month']
-                if 'emsc' in self.context.job_params:
-                    bccvlmd['emsc'] = self.context.job_params['emsc']
-                if 'gcm' in self.context.job_params:
-                    bccvlmd['gcm'] = self.context.job_params['gcm']
                 # exp.future_climate_datasets()
-            elif genre == 'DataGenreSDMEval' and info.get('mimetype') == 'text/csv':
-                if thresholds:
-                    bccvlmd['thresholds'] = thresholds
+            if genre == 'DataGenreSDMEval' and bccvlmd.get('thresholds', None):  # only for mimetype text/csv
+                # convert thresholds to Decimal
+                for key, value in bccvlmd['thresholds'].items():
+                    try:
+                        bccvlmd['thresholds'][key] = Decimal(value)
+                    except (TypeError, InvalidOperation) as e:
+                        LOG.warn("Couldn't parse threshold vlaue '%s' (%s) from file '%s': %s", name, key, url, repr(e))
 
-        mimetype = info.get('mimetype', None)
-        if mimetype is None:
-            mimetype = guess_mimetype(fname, mtr)
+        mimetype = import_item['file']['contenttype']
+        # mimetype = info.get('mimetype', None)
+        # if mimetype is None:
+        #     mimetype = guess_mimetype(fname, mtr)
 
-        fileinfo = { 'name': name, 'path': fname }
-        if urlparse(fname).scheme == 'file':
-            # Dataset stored locally in plone
-            datasettype = 'org.bccvl.content.dataset'
-            fileinfo['data'] = open(fname, 'r')
+        # build item
+        item = {
+            '_path': datasetid,
+            'title': unicode(name),
+            'description': import_item.get('title', u''),
+            'bccvlmetadata': bccvlmd,
+            '_layermd': import_item.get('layermd', {})
+        }
+
+        # dataset or remotedataset?
+        urlparts = urlsplit(import_item['file']['url'])  # params resultsfolder
+        # TODO decide local remote here or use import_item['file']['url']
+        if 'http' in urlparts.scheme:
+            # remote
+            remoteurl = re.sub(r'^swift\+', '', import_item['file']['url'])
+            item.update({
+                '_type': 'org.bccvl.content.remotedataset',
+                'remoteUrl': remoteurl,
+                # FIXME: hack to pass on content type to FileMetadataToBCCVL blueprint
+                '_files': {
+                    remoteurl: {
+                        'contenttype': mimetype,
+                    }
+                },
+                '_filemetadata': {
+                    # key relates to 'remoteUrl'
+                    remoteurl: import_item.get('filemetadata', {}) or {},  # make sure it's not none
+                }
+            })
         else:
-            # Dataset stored in remote host
-            datasettype = 'org.bccvl.content.remotedataset'
+            # assume local storage
+            item.update({
+                '_type': 'org.bccvl.content.dataset',
+                'file': {
+                    'file': name,
+                    'contenttype': mimetype,
+                    'filename': name
+                },
+                '_files': {
+                    name: {
+                        'name': name,
+                        'path': urlparts.path,
+                        'data': open(urlparts.path, 'r')
+                    }
+                },
+                '_filemetadata': {
+                    # key relates to 'file':'file'
+                    name: import_item.get('filemetadata', {}) or {},  # make sure it's not none
+                }
+            })
 
         LOG.info("Ingest item: %s, %s", datasetid, name)
-        return {
-            '_path': datasetid,
-            '_type': datasettype,
-            'title': unicode(name),
-            'description': info.get('title', u''),
-            'file': {
-                'file': name,
-                'contenttype': mimetype,
-                'filename': name
-            },
-            'bccvlmetadata': bccvlmd,
-            # FIXME: I think adding open file descriptors works just fine,
-            #        otherwise could use new 'path' feature with additional blueprint
-            '_files': {
-                name: fileinfo
-            },
-            '_filemetadata': {
-                    name: md
-            },
-            '_layermd': layermd
-        }
+        return item
 
     def __iter__(self):
         # exhaust previous iterator
@@ -187,8 +184,8 @@ class ResultSource(object):
             yield item
 
         # Import each item
-        for urlpath, mdinfo in self.filesmd.items():
-            item = self.createItem(urlpath, mdinfo['fileinfo'], mdinfo['thresholds'])
+        for import_item in self.items:
+            item = self.createItem(import_item)
             yield item
 
 
