b2e3aa0cbecfec36fa72d595d96b356faf658bdc
Yong Liaw <y.liaw@griffith.edu.au>
2016-11-09 17:26:07 +1000

    BCCVLB-577: Update R-scripts to read in trait dataset and setup to run algorithm and save output.

--- src/org/bccvl/compute/content/toolkit/speciestrait_cta/speciestrait_cta.R
+++ src/org/bccvl/compute/content/toolkit/speciestrait_cta/speciestrait_cta.R
@@ -8,18 +8,12 @@
 ## trait dataset csv file
 trait.data.filename = bccvl.params$traits_dataset$filename
 # mapping of variable names of trait dataset
-trait.data.varnames = bccvl.params$traits_dataset_params
+trait.data.params = bccvl.params$traits_dataset_params
 
-# read in the trait data
+# read in the trait data as data frame
 trait.data = read.csv(trait.data.filename)
-# Loop through the trait data variables name to extract trait and env data
-for (varname in ls(trait.data.varnames)) {
-    if (varname %in% colnames(trait.data)) {
-      assign(paste(varname), trait.data[,varname])
-    }
-}
 
-env.data <- bccvl.params$traits_dataset_params$EnvVar1 # CH: same question, how do we make sure we select all env variables selected here?
+# TODO: Read in other env variables
 
 ## Set parameters (need to be adjusted to link to back end bccvl.params file)
 
@@ -47,62 +41,65 @@ gen_formulae <- function(dataset_params) {
     envvars = paste(names(cols[['env']]), collapse=' + ')
     for (trait in names(cols[['trait']])) {
         formulae = append(formulae, list(list(formula=paste(trait, '~', envvars),
-                                         method=ifelse(cols[['trait']][trait] == 'categorical', 'class', 'anova'))))
+                                         method=ifelse(cols[['trait']][trait] == 'categorical', 'class', 'anova'), trait=trait)))
     }
-    # return a list of lists, where each sublist has $formula and $method elements
+    # return a list of lists, where each sublist has $formula, $method elements, and $trait
     return (formulae)
 }
-# CH: do we preset the formula in the params file, or do we need to write a loop here so the model will be run for each trait?
-# CH: as in comments below the method is the only configuration option that is different for categorical vs continuous traits - this
-# should be fixed by us and not changeable by user
 
-trait.cta.options <- list(formula = bccvl.params$formula, # formula should be: trait ~ env1 + env2 + env3 etc 
-                          method = bccvl.params$method, # should be "class" for categorical trait data, and "anova" for continuous trait data
-                          na.action = na.rpart, # default action deletes observations for which trait value is missing, but keeps those in which one or more environmental variables are missing
-                          model = FALSE,
-                          x = FALSE,
-                          y = FALSE,
-                          control = list(minsplit = bccvl.params$control_minsplit, #the minimum number of observations that must exist in a node in order for a split to be attempted
-                                         minbucket = bccvl.params$control_minbucket, #the minimum number of observations in any terminal node
-                                         cp = bccvl.params$control_cp, #complexity parameter
-                                         maxcompete = bccvl.params$control_maxcompete, # number of competitor splits retained in the output
-                                         maxsurrogate = bccvl.params$control_maxsurrogate, # number of surrogate splits retained in the output
-                                         usesurrogate = bccvl.params$control_usesurrogate, # how to use surrogates in splitting process
-                                         surrogatestyle = bccvl.params$control_surstyle, # controls the selection of a best surrogate
-                                         xval = bccvl.params$control_xval, #number of cross-validations
-                                         maxdepth = bccvl.params$control_maxdepth  #Set the maximum depth of any node of the final tree, with the root node counted as depth 0. Values greater than 30 rpart will give nonsense results on 32-bit machines
-                                         )
-                          )
+# Genrate formula for each trait, and run each formula
+formulae = gen_formulae(trait.data.params)
+for (formula in formulae) {
+    trait_name = formula$trait
+    trait.cta.options <- list(formula = formula(formula$formula), # formula should be: trait ~ env1 + env2 + env3 etc 
+                              method = formula$method, # should be "class" for categorical trait data, and "anova" for continuous trait data
+                              na.action = na.rpart, # default action deletes observations for which trait value is missing, but keeps those in which one or more environmental variables are missing
+                              model = FALSE,
+                              x = FALSE,
+                              y = FALSE,
+                              control = list(minsplit = bccvl.params$control_minsplit, #the minimum number of observations that must exist in a node in order for a split to be attempted
+                                             minbucket = bccvl.params$control_minbucket, #the minimum number of observations in any terminal node
+                                             cp = bccvl.params$control_cp, #complexity parameter
+                                             maxcompete = bccvl.params$control_maxcompete, # number of competitor splits retained in the output
+                                             maxsurrogate = bccvl.params$control_maxsurrogate, # number of surrogate splits retained in the output
+                                             usesurrogate = bccvl.params$control_usesurrogate, # how to use surrogates in splitting process
+                                             surrogatestyle = bccvl.params$control_surstyle, # controls the selection of a best surrogate
+                                             xval = bccvl.params$control_xval, #number of cross-validations
+                                             maxdepth = bccvl.params$control_maxdepth  #Set the maximum depth of any node of the final tree, with the root node counted as depth 0. Values greater than 30 rpart will give nonsense results on 32-bit machines
+                                             )
+                              )
 
-# Default values for control parameters:
-# minsplit = 20
-# minbucket = round(minsplit/3)
-# cp = 0.01
-# maxcompete = 4
-# maxsurrogate = 5
-# usesurrogate = 2
-# surrogatestyle = 0
-# xval = 10
-# maxdepth = 30
+    # Default values for control parameters:
+    # minsplit = 20
+    # minbucket = round(minsplit/3)
+    # cp = 0.01
+    # maxcompete = 4
+    # maxsurrogate = 5
+    # usesurrogate = 2
+    # surrogatestyle = 0
+    # xval = 10
+    # maxdepth = 30
 
-## Run rpart model
+    ## Run rpart model
+    trait.cta = rpart(formula = trait.cta.options$formula,
+                      data = trait.data, # data frame containing trait and env data
+                      method = trait.cta.options$method,
+                      na.action = trait.cta.options$na.action,
+                      model = trait.cta.options$model,
+                      x = trait.cta.options$x,
+                      y = trait.cta.options$y,
+                      control = trait.cta.options$control)
 
-trait.cta = rpart(formula = trait.cta.options$formula,
-                  data = ,#?? CH: how to link to data?
-                  method = trait.cta.options$method,
-                  na.action = trait.cta.options$na.action,
-                  model = trait.cta.options$model,
-                  x = trait.cta.options$x,
-                  y = trait.cta.options$y,
-                  control = trait.cta.options$control)
+    ### Save the results as text to file for each trait
+    s <- summary(trait.cta) # saved and displayed as text
+    bccvl.write.text(s, paste0(trait_name, ".cta.results.txt"))
+    p <- printcp(trait.cta) # saved and displayed as text
+    bccvl.write.text(p, paste0(trait_name, ".cta.results.txt"), append=TRUE)
 
-### Save the results to file
+    # save the plot as png image
+    ofilename = paste0(trait_name, ".cta.plotcp")
+    bccvl.write.image(trait.cta, ofilename, "plotcp")
 
-summary(trait.cta) # CH: this should be saved as a result and displayed as txt file
-printcp(trait.cta) # CH: this should be saved as a result and displayed as table (or txt file if easier for now)
-plot(trait.cta) # CH: this should be saved as a result and displayed as figure
-
-bccvl.save(trait.cta, "trait.cta.object.RData")
-
-
-
+    # Save the model
+    bccvl.save(trait.cta, paste0(trait_name, ".cta.object.RData"))
+}
