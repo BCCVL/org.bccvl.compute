9a9390301198ab58fbd8d160b43ec17927f5f86b
Chantal Huijbers <c.huijbers@griffith.edu.au>
2016-11-11 12:24:08 +1000

    Update speciestrait_glm.R

--- src/org/bccvl/compute/content/toolkit/speciestrait_glm/speciestrait_glm.R
+++ src/org/bccvl/compute/content/toolkit/speciestrait_glm/speciestrait_glm.R
@@ -5,91 +5,103 @@
 
 ### Runs a Generalized Linear Model to test the effect of selected environmental variables on species traits
 
-## trait dataset csv file
+## DATA
+
+# Link to input dataset csv file
 trait.data.filename = bccvl.params$traits_dataset$filename
-# mapping of variable names of trait dataset
+# Link to variable names of input dataset
 trait.data.varnames = bccvl.params$traits_dataset_params
 
-# read in the trait data
+# Read in the trait data
 trait.data = read.csv(trait.data.filename)
-# Loop through the trait data variables name to extract trait and env data
+# Loop through the trait data variables names to extract trait and environmental data
 for (varname in ls(trait.data.varnames)) {
     if (varname %in% colnames(trait.data)) {
       assign(paste(varname), trait.data[,varname])
     }
 }
 
-env.data <- bccvl.params$traits_dataset_params$EnvVar1 # CH: same question, how do we make sure we select all env variables selected here?
+# TODO: Read in other env variables
 
-## CH: below is old script
+## MODEL
 
-## Set up the function call expression # CH: Yong, Gerhard is this still correct?
-glm.params = list(data=glm.data)
-
-## set defaults for missing parameters according to R docs:
-## see Jon Shuker's inputs specification for reference
-glm.defaults = list(family="gaussian(link=identity)",
-                   subset=NULL,
-                   weights=NULL,
-                   na.action=options("na.action")[[1]],
-                   start=NULL,
-                   ea_start=NULL,
-                   mu_start=NULL,
-                   offset=NULL,
-                   method="glm.fit",
-                   model=TRUE,
-                   x=FALSE,
-                   y=FALSE,
-                   contrasts=NULL)
-
-## CH: Yong, do we still need lines below (46-73) if we refer to params file in the function that runs the model???
-# plain old parameters
-for (paramname in c('formula', 'family', 'na.action', 'method', 'model', 'x', 'y')) {
-    if (! is.null(bccvl.params[[paramname]])) {
-        glm.params[paramname] = bccvl.params[paramname]
-    } else {
-        glm.params[paramname] = glm.defaults[paramname]
+# Generate formulae for additive models that test the response of each trait (1 per model) to all environmental variables selected
+# e.g. trait ~ env1 + env2 + env3 etc.
+gen_formulae <- function(dataset_params) {
+  cols = list(species=list(),
+              lat=list(),
+              lon=list(),
+              env=list(),
+              trait=list())
+  for(colname in names(dataset_params)) {
+    colval = dataset_params[[colname]]
+    if (colval == 'species' || colval == 'lon' || colval == 'lat') {
+      cols[[colval]][colname] = colval
+    } else if (colval == 'env_var_cat') {
+      cols[['env']][colname] = 'categorical'
+    } else if (colval == 'env_var_con') {
+      cols[['env']][colname] = 'continuous'
+    } else if (colval == 'trait_ord') {
+      cols[['trait']][colname] = 'ordinal'
+    } else if (colval == 'trait_nom') {
+      cols[['trait']][colname] = 'nominal'
+    } else if (colval == 'trait_con') {
+      cols[['trait']][colname] = 'continuous'
     }
+  }
+  formulae = list()
+  envvars = paste(names(cols[['env']]), collapse=' + ')
+  for (trait in names(cols[['trait']])) {
+    formulae = append(formulae, list(list(formula=paste(trait, '~', envvars),
+  }
+  # return a list of lists, where each sublist has $formula and $trait
+  return (formulae)
 }
 
-# parameters that sholud refer to a column in glm.data ## CH: these parameters are now hidden for user to change, so we can probably 
-# get rid of lines below:
-for (paramname in c('start', 'eta_start', 'mu_start', 'subset', 'weights', 'contrasts','offset')) {
-    if (! is.null(bccvl.params[[paramname]])) {
-        glm.params[paramname] = glm.data[bccvl.params[[paramname]]]
-    } else {
-        glm.params[paramname] = glm.defaults[paramname]
-    }
-}
+# Run model
 
-# singular.ok has a different name in bccvl.params
-if (! is.null(bccvl.params['singular_ok'])) {
-    glm.params['singular.ok'] = bccvl.params$singular_ok
-} else {
-    glm.params['singular.ok'] = glm.defaults$singular.ok
-}
+formulae = gen_formulae(trait.data.params)
+for (formula in formulae) {
+  trait_name = formula$trait
 
-# parse the family string (character) into a proper family object
-glm.params$family=family_from_string(glm.params$family)
+# TODO: include script/loop to run different function for different categories:
 
+# for continuous traits:
+trait.glm = glm(formula=formula(formula$formula),
+                family=bccvl.params$family,
+                data= # TODO: do we need another link to the data here?
+                weights=NULL,
+                na.action=bccvl.params$na.action,
+                start=NULL,
+                etastart=NULL,
+                mustart=NULL,
+                offset=NULL,
+                model=, #CH check whether this should be true or false
+                method=bccvl.params$method,
+                x=FALSE,
+                y=FALSE,
+                contrasts=NULL)    
 
-## Run the regression
-glm.result = glm(formula=glm.params$formula, ## formula should be: trait ~ env1 + env2 + env3 etc 
-                 family=glm.params$family,
-                 data=glm.params$data,
-                 weights=glm.params$weights,
-                 subset=glm.params$subset,
-                 na.action=glm.params$na.action[[1]],
-                 start=glm.params$start,
-                 etastart=glm.params$eta_start,
-                 mustart=glm.params$mu_start,
-                 offset=glm.params$offset,
-                 model=glm.params$model,
-                 method=glm.params$method,
-                 x=glm.params$x,
-                 y=glm.params$y,
-                 contrasts=glm.params$contrasts)
+# for ordinal traits:    
+trait.polr = polr(formula=formula(formula$formula),
+                  data= # TODO: do we need another link to the data here?
+                  weights=NULL,
+                  na.action=bccvl.params$na.action,
+                  contrasts=NULL,
+                  model=, #CH check whether this should be true or false
+                  method="logistic")   
 
+# for nominal traits:    
+trait.multinom = multinom(formula=formula(formula$formula),
+                  data= # TODO: do we need another link to the data here?
+                  weights=NULL,
+                  na.action=bccvl.params$na.action,
+                  contrasts=NULL,
+                  summ=0,        
+                  model=,) #CH check whether this should be true or false
+    
+                                                  
+# TODO:                              
 ## Save the result to file
 
 bccvl.save(glm.result, "glm.model.object.RData")
