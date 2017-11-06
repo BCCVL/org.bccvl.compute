9043817a79b615dc7b4a154d39f44046409f13c2
Yong Liaw <y.liaw@griffith.edu.au>
2017-11-06 13:28:30 +1000

    BCCVL-61: Overwrite the SDMTools functions distance and grid.info to fix issue due to floating point operations.

--- src/org/bccvl/compute/rscripts/bccvl.R
+++ src/org/bccvl/compute/rscripts/bccvl.R
@@ -1020,3 +1020,150 @@ family_from_string <- function(s)
     }
     return (do.call(what=f, args=args))
 }
+
+#' Grid Information from Geographic (lat lon) Projections
+#' 
+#' Since spatial grids in geographic projections do not have equal area or
+#' perimeters, \code{grid.info} extracts perimeter & area related information
+#' for latitudinal bands with differing longitudinal widths. \cr\cr Outputs
+#' lengths are in m using Vincenty's equation (\code{distance})and areas in m2.
+#' Surface areas are calculated summing surface areas of spherical polygons as
+#' estimated using l'Huiller's formula.
+#' 
+#' 
+#' @param lats is a vector of latitudes representing the midpoint of grid cells
+#' @param cellsize is a single value (assuming square cells) or a two value
+#' vector (rectangular cells) representing the height (latitude) and width
+#' (longitude) of the cells
+#' @param r is a single value representing the radius of the globe in m.
+#' Default is for the WGS84 elipsoid
+#' @return a data.frame listing: \item{lat}{the latitude representing the
+#' midpoint of the cell} \item{top}{length of the top of the cell (m)}
+#' \item{bottom}{length of the bottom of the cell (m)} \item{side}{length of
+#' the side of the cell (m)} \item{diagnal}{length of the diagnals of the cell
+#' (m)} \item{area}{area of the cell (m2)}
+#' @author Jeremy VanDerWal \email{jjvanderwal@@gmail.com}
+#' @references information on l'Huiller's formula
+#' \url{http://williams.best.vwh.net/avform.htm for more info)} code for
+#' estimating area of polygon on sphere was modified from
+#' \url{http://forum.worldwindcentral.com/showthread.php?t=20724}
+#' @examples
+#' 
+#' #show output for latitudes from -87.5 to 87.5 at 5 degree intervals
+#' grid.info(lats=seq(-87.5,87.5,5), 5)
+#' 
+#' @export 
+# This is a fix for SDM tool grid.info function due to floating point operation.
+grid.info <- function(lats,cellsize,r=6378137) {
+    r2 = r^2 #radius of earth
+    ###need checks to ensure lats will not go beyond 90 & -90
+    if (length(cellsize)==1) cellsize=rep(cellsize,2) #ensure cellsize is defined for both lat & lon
+    out = data.frame(lat=lats) #setup the output dataframe
+    toplats = lats+(0.5*cellsize[1]); bottomlats = lats-(0.5*cellsize[1]) #define the top and bottom lats
+    check = range(c(toplats,bottomlats),na.rm=TRUE); if (-90.00001>check[1] | 90.00001<check[2]) stop('latitudes must be between -90 & 90 inclusively')
+    out$top = distance(toplats,rep(0,length(lats)),toplats,rep(cellsize[2],length(lats)))$distance
+    out$bottom = distance(bottomlats,rep(0,length(lats)),bottomlats,rep(cellsize[2],length(lats)))$distance
+    out$side = distance(toplats,rep(0,length(lats)),bottomlats,rep(0,length(lats)))$distance
+    out$diagnal = distance(toplats,rep(0,length(lats)),bottomlats,rep(cellsize[2],length(lats)))$distance
+    #calculate area of a spherical triangle using spherical excess associated by knowing distances
+    #tan(E/4) = sqrt(tan(s/2)*tan((s-a)/2)*tan((s-b)/2)*tan((s-c)/2))
+    #where a, b, c = sides of spherical triangle
+    #s = (a + b + c)/2
+    #from CRC Standard Mathematical Tables
+    #calculate excess based on  l'Huiller's formula (http://williams.best.vwh.net/avform.htm for more info)
+    #code modified from (http://forum.worldwindcentral.com/showthread.php?t=20724)
+    excess = function(lam1,lam2,beta1,beta2){ #calculate excess... inputs are in radians
+        haversine = function(y) { (1-cos(y))/2 }
+        cosB1 = cos(beta1); cosB2 = cos(beta2)
+        hav1 = haversine(beta2-beta1) + cosB1*cosB2*haversine(lam2-lam1)
+        aa = 2 * asin(sqrt(hav1)); bb = 0.5*pi - beta2; cc = 0.5*pi - beta1
+        ss = 0.5*(aa+bb+cc)
+        tt = tan(ss/2)*tan((ss-aa)/2)*tan((ss-bb)/2)*tan((ss-cc)/2)
+        return(abs(4*atan(sqrt(abs(tt)))))      
+    }
+    if (any(bottomlats==-90)) { pos = which(bottomlats==-90); bottomlats[pos] = -bottomlats[pos]; toplats[pos] = -toplats[pos]} #ensure no -90 bottom lats
+    out$area = excess(lam1=0,lam2=cellsize[2]*pi/180,toplats*pi/180,toplats*pi/180)
+    out$area = abs(out$area-excess(lam1=0,lam2=cellsize[2]*pi/180,bottomlats*pi/180,bottomlats*pi/180))*r2
+    return(out)
+}
+
+#' Vincenty Direct Calculation of Distance and Direction
+#' 
+#' \code{distance} estimates the distance given a starting & ending latitude
+#' and longitude. \cr \cr For general information on Vincenty's formula, see
+#' e.g., \url{http://en.wikipedia.org/wiki/Vincenty's_formulae}. It states: \cr
+#' \emph{Vincenty's formulae are two related iterative methods used in geodesy
+#' to calculate the distance between two points on the surface of an spheroid,
+#' developed by Thaddeus Vincenty in 1975. They are based on the assumption
+#' that the figure of the Earth is an oblate spheroid, and hence are more
+#' accurate than methods such as great-circle distance which assume a spherical
+#' Earth.} \cr \cr \bold{Note:} this method assumes a locations are lat & lon
+#' given in WGS 84.\cr\cr Direction, if requested, is the the initial bearing
+#' (sometimes referred to as forward azimuth) for which one would follow as a
+#' straight line along a great-circle arc from start to finish.\cr \cr
+#' \bold{Note:} this will fail if there are NA's in the data.
+#' 
+#' 
+#' @param lat1 a single value or vector of values representing latitude in
+#' decimal degrees from -90 to 90 degrees. Alternatively, a data.frame or
+#' matrix can be used here with each column representing lat1, lon1, lat2, lon2
+#' (in that order).
+#' @param lon1 a single value or vector of values representing longitude in
+#' decimal degrees from -180 to 180 degrees. If NULL, lat1 is assumed to be a
+#' matrix or data.frame.
+#' @param lat2 a single value or vector of values representing latitude in
+#' decimal degrees from -90 to 90 degrees. If NULL, lat1 is assumed to be a
+#' matrix or data.frame.
+#' @param lon2 a single value or vector of values representing longitude in
+#' decimal degrees from -180 to 180 degrees. If NULL, lat1 is assumed to be a
+#' matrix or data.frame.
+#' @param bearing boolean value as to calculate the direction as well as the
+#' distance.
+#' @return Returns a data.frame with: \item{lon1}{the original longitude}
+#' \item{lat1}{the original latitude} \item{lon2}{the destination longitude}
+#' \item{lat2}{the destination latitude} \item{distance}{the distance used}
+#' \item{bearing}{if requested, the bearing between the two points}
+#' @author Jeremy VanDerWal \email{jjvanderwal@@gmail.com}
+#' @seealso \code{\link{destination}}
+#' @references Vincenty, T. 1975. Direct and Inverse Solutions of Geodesics on
+#' the Ellipsoid with application of Nested Equations. Survey Review, vol XXII
+#' no 176. \url{http://www.ngs.noaa.gov/PUBS_LIB/inverse.pdf}
+#' @source The source code for the distance algorithm here was modified from
+#' \url{http://www.movable-type.co.uk/scripts/latlong-vincenty.html}.\cr \cr
+#' Distances were validated against Geoscience Australia calculations
+#' (\url{http://www.ga.gov.au/geodesy/datums/vincenty_inverse.jsp}).\cr \cr
+#' Bearings were from multiple sources including
+#' \url{http://williams.best.vwh.net/avform.htm#Crs}.
+#' @examples
+#' 
+#' 
+#' #get the distance of 1 degree longitude at each 5 degrees latitude from -90 to 90
+#' distance(lat1=seq(-90,90,5),lon1=rep(0,37),lat2=seq(-90,90,5),lon2=rep(1,37),bearing=TRUE)
+#'  
+#' 
+#' @export 
+#' @useDynLib SDMTools Dist
+# This is a fix for SDM tool distance function due to floating point operation.
+distance = function(lat1, lon1=NULL, lat2=NULL, lon2=NULL, bearing=FALSE) {
+    if (is.data.frame(lat1) | is.matrix(lat1)) { #if input is matrix or data.frame... break it out to individual vectors
+        lat1 = as.matrix(lat1); if (ncol(lat1)!=4) stop('incorrect lat/lon inputs... must be matrix with 4 columns or 4 vectors')
+        lon2=lat1[,4]; lat2=lat1[,3]; lon1=lat1[,2]; lat1=lat1[,1] #break out individual columns
+    } else if (!is.null(lat2) & !is.null(lon1) & !is.null(lon2)) {
+        if (!all(c(length(lat2),length(lon1),length(lon2))==length(lat1))) stop('inputs must all be of same length')
+    } else { stop('inappropriate inputs... see helpfile') }
+    if (any(c(lon1,lon2) < -180.00001) | any(c(lon1,lon2) > 180.00001)) stop('lon must be decimal degrees between -180 & 180')
+    if (any(c(lat1,lat2) < -90.00001) | any(c(lat1,lat2) > 90.00001)) stop('lat must be decimal degrees between -90 & 90')
+    #cycle through and output the new data
+    out = data.frame(lat1=lat1,lon1=lon1,lat2=lat2,lon2=lon2)
+    out$distance = round(.Call('Dist',out$lat1,out$lon1,out$lat2,out$lon2,PACKAGE='SDMTools'),2) #round to the nearest mm
+    if (bearing) { #if requested, calculate bearing
+        lat1=lat1*pi/180;lat2=lat2*pi/180;lon1=lon1*pi/180;lon2=lon2*pi/180 #convert to radians
+        brng = atan2(sin(lon2-lon1)*cos(lat2),cos(lat1)*sin(lat2)-sin(lat1)*cos(lat2)*cos(lon1-lon2)) #estimate bearing
+        out$bearing = ((brng*180/pi)+360)%%360 #convert to bearing in degrees
+    }
+    #return the output
+    return(out)
+}
+
+assignInNamespace("grid.info",grid.info, ns="SDMTools")
+assignInNamespace("distance",distance, ns="SDMTools")
