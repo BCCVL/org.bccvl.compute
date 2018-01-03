dd9370f90ca2a275d3f8bfafd902777f6faaf2dd
Chantal Huijbers <c.huijbers@griffith.edu.au>
2018-01-03 13:29:58 +1000

    Add 'd' as configuration option
    BCCVL-125

--- src/org/bccvl/compute/content/toolkit/circles/circles.R
+++ src/org/bccvl/compute/content/toolkit/circles/circles.R
@@ -10,10 +10,10 @@
 ##
 ##  outputdir ... root folder for output data
 
-#define the working environment
-#scriptdir = normalizePath(bccvl.env$scriptdir)
-#inputdir =  normalizePath(bccvl.env$inputdir)
-#outputdir =  normalizePath(bccvl.env$outputdir)
+# define the working environment
+# scriptdir = normalizePath(bccvl.env$scriptdir)
+# inputdir =  normalizePath(bccvl.env$inputdir)
+# outputdir =  normalizePath(bccvl.env$outputdir)
 
 
 # extract params
@@ -21,31 +21,31 @@
 occur.data = bccvl.params$species_occurrence_dataset$filename
 occur.species = bccvl.params$species_occurrence_dataset$species
 occur.filter = bccvl.params$species_filter
-#define the the lon/lat of the background / psuedo absence points to use -- 2 column matrix of longitude and latitude
+# define the the lon/lat of the background / psuedo absence points to use -- 2 column matrix of longitude and latitude
 absen.data = bccvl.params$species_absence_dataset$filename
-#define the current enviro data to use
+# define the current enviro data to use
 enviro.data.current = lapply(bccvl.params$environmental_datasets, function(x) x$filename)
-#type in terms of continuous or categorical
+# type in terms of continuous or categorical
 enviro.data.type = lapply(bccvl.params$environmental_datasets, function(x) x$type)
-#layer names for the current environmental layers used
+# layer names for the current environmental layers used
 enviro.data.layer = lapply(bccvl.params$environmental_datasets, function(x) x$layer)
-#geographic constraints
+# geographic constraints
 enviro.data.constraints = bccvl.params$modelling_region
-#Indicate to generate and apply convex-hull polygon of occurrence dataset to constraint
+# indicate to generate and apply convex-hull polygon of occurrence dataset to constraint
 enviro.data.generateCHall = ifelse(is.null(bccvl.params$generate_convexhull), FALSE, as.logical(bccvl.params$generate_convexhull))
 # resampling (up / down scaling) if scale_down is TRUE, return 'lowest'
 enviro.data.resampling = ifelse(is.null(bccvl.params$scale_down) ||
                                 as.logical(bccvl.params$scale_down),
                                 'highest', 'lowest')
 
-#additional parameters for projecting circles
-opt.tails = bccvl.params$tails # default "both"; use to ignore the left or right tail of the percentile distribution ("both", "low", "high"
-opt.ext = NULL #an optional extent object to limit the prediction to a sub-region of 'x'
+# additional parameters for 'Circles' algorithm
+opt.d = bccvl.params$d # radius around circles, if not specified it is computed from the mean inter-point distance 
+opt.tails = bccvl.params$tails # default "both"; use to ignore the left or right tail of the percentile distribution ("both", "low", "high")
+opt.ext = NULL # an optional extent object to limit the prediction to a sub-region of 'x'
 projection.name = "current"
 species_algo_str = ifelse(is.null(bccvl.params$subset), 
                           sprintf("%s_circle", occur.species), 
                           sprintf("%s_circle_%s", occur.species, bccvl.params$subset))
-
 
 # model accuracy statistics
 # these are available from dismo::evaluate.R NOT originally implemented in biomod2::Evaluate.models.R
@@ -59,7 +59,7 @@ model.accuracy = c(dismo.eval.method, biomod.models.eval.meth)
 # read current climate data
 current.climate.scenario = bccvl.enviro.stack(enviro.data.current, enviro.data.type, enviro.data.layer, resamplingflag=enviro.data.resampling)
 
-###read in the necessary observation, background and environmental data
+# read in the necessary observation, background and environmental data
 occur = bccvl.species.read(occur.data, occur.filter) #read in the observation data lon/lat
 # keep only lon and lat columns
 occur = occur[c("lon","lat")]
@@ -68,17 +68,16 @@ occur = occur[c("lon","lat")]
 if (!is.null(enviro.data.constraints) || enviro.data.generateCHall) {
   constrainedResults = bccvl.sdm.geoconstrained(current.climate.scenario, occur, absen.data, enviro.data.constraints, enviro.data.generateCHall);
 
-  # Save a copy of the climate dataset
+# save a copy of the climate dataset
   current.climate.scenario.orig <- current.climate.scenario  
   current.climate.scenario <- constrainedResults$raster
   occur <- constrainedResults$occur
 }
 
-
-# Circle algorithm does not need pseudo absence points
+# Circles algorithm does not need pseudo-absence points
 pa_number_point = 0
 
-# Format the data as in biomod2. This will also generate the psedo absence points.
+# format the data as in biomod2. This will also generate the psedo absence points.
 biomod2.data = bccvl.biomod2.formatData(absen.filename   = absen.data,
                                   pseudo.absen.points    = pa_number_point,
                                   pseudo.absen.strategy  = bccvl.params$pa_strategy,
@@ -100,30 +99,31 @@ occur = coord[c(which(biomod2.data@data.species == 1)), names(coord)]
 #
 ###############
 
-# circles(p, ...)
+# circles(p, d, ...)
 # p point locations (presence), two column matrix, data.frame or SpatialPoints* object
 # d the radius of each circle in meters; a single number or a vector with elements corresponding to
 #   rows in 'p'; if missing the diameter is computed from the inter-point distance
 # n how many vertices in the circle? default is 360
-# lonlat are these longitude/latitude data? default value is false
+# lonlat logical, are these longitude/latitude data? 
 # r radius of the earth; only relevant for longitude/latitude data; default is 6378137 m
 
 if (!all(enviro.data.type=="continuous")) {
     warning("circles not run because categorical data cannot be used")
 } else {
     # run circles with matrix of enviro data
-    model.sdm = circles(p=occur, lonlat=TRUE)
-    # save out the model object
+    model.sdm = circles(p=occur, d=opt.d, lonlat=TRUE)
+    
+  # save out the model object
     bccvl.save(model.sdm, bccvl.format.outfilename(filename="model.object", id_str=species_algo_str, ext="RData"))
 
-    # Do projection over current climate scenario without constraint
+    # do projection over current climate scenario without constraint
     if (!is.null(enviro.data.constraints) || enviro.data.generateCHall) {
         model.proj = predict(model.sdm, current.climate.scenario.orig@layers[[1]], mask=TRUE)
 
-        # remove the current.climate.scenario to release disk space
+     # remove the current.climate.scenario to release disk space
         bccvl.remove.rasterObject(current.climate.scenario.orig)
 
-        # save output
+     # save output
         bccvl.saveModelProjection(model.proj, projection.name, occur.species, species_algo_str, filename_ext="unconstraint")
     }
 
